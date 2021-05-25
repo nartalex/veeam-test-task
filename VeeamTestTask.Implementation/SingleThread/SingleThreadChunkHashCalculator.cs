37154156fb63fa492a93b464630fa0ad97ea34e1
Artem Naruzhniy <NaruzhniyAA@sibedge.com>
2021-05-25 23:31:04 +0300

    Исправление однопоточной реализации

--- VeeamTestTask.Implementation/SingleThread/SingleThreadChunkHashCalculator.cs
+++ VeeamTestTask.Implementation/SingleThread/SingleThreadChunkHashCalculator.cs
@@ -17,31 +17,33 @@ namespace VeeamTestTask.Implementation.SingleThread
         /// <inheritdoc/>
         public void SplitFileAndCalculateHashes(Stream fileStream, int blockSize, string hashAlgorithmName, IBufferedResultWriter resultWriter)
         {
-            // Это позволяет нам не создавать слишком большой массив буффера,
-            // если файл сам по себе меньше размера блока
             var bytesLeft = fileStream.Length;
-            if (bytesLeft < blockSize)
-            {
-                Debug.WriteLine($"File length is lower than block size, new block size is {bytesLeft} b");
-                blockSize = (int)bytesLeft;
-            }
-
             byte[] buffer = new byte[blockSize];
             var chunkIndex = 1;
             var numberOfBytes = 0;
             using var hashAlgorithm = HashAlgorithm.Create(hashAlgorithmName);
 
-            while ((numberOfBytes = fileStream.Read(buffer, 0, blockSize)) != 0)
+            while (true)
             {
-                // Если мы находимся в последнем блоке, часть массива будет занята нулями
-                // Чтобы не расчитывать хэш для нулевой части, укажем явно границы массива
-                if (blockSize > numberOfBytes)
+                // Это позволяет нам не создавать слишком большой массив буффера, если файл сам по себе меньше размера блока
+                // А так же, если мы находимся в последнем блоке, часть массива будет занята нулями,
+                // чтобы не расчитывать хэш для нулевой части, укажем явно границы массива
+                if (bytesLeft < blockSize)
                 {
-                    buffer = buffer[0..numberOfBytes];
+                    blockSize = (int)bytesLeft;
                 }
 
-                var hashBytes = hashAlgorithm.ComputeHash(buffer);
-                resultWriter.Write(chunkIndex++, hashBytes);
+                numberOfBytes = fileStream.Read(buffer, 0, blockSize);
+                if(numberOfBytes == 0)
+                {
+                    break;
+                }
+
+                var hashBytes = hashAlgorithm.ComputeHash(buffer, 0, blockSize);
+                resultWriter.Write(chunkIndex, hashBytes);
+
+                bytesLeft = fileStream.Length - fileStream.Position;
+                chunkIndex++;
             }
         }
     }
